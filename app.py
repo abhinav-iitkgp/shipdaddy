5fd8edc93e59c6815e0c6f726695117ceb4f276b
abhinav-iitkgp <abhinav.singh2029@gmail.com>
2023-05-19 16:04:47 +0530

    v1 with chat

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+from streamlit_chat import message
+from utils import get_initial_message, get_chatgpt_response, update_chat
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import openai
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+st.set_page_config(page_title="Ship Daddy", page_icon=":robot:")
+st.header("SHIP DADDY V1")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown("Just Ask for the data you want about the properties table of Nobroker. This WebApp will create a custom sql query for you as per your request. If you dont get the desired output just tell it what's wrong, ShipDaddy will correct itself.\n\n Cheers:)")
+
+with col2:
+    st.image(image='shipdaddy_logo_500.png', width=500)
+
+st.markdown("## What data do you want from property table?")
+
+model = "gpt-3.5-turbo"
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+query = st.text_input("Ask Me: 🤗 ", key="input")
+
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = get_initial_message()
+
+
+if query:
+    with st.spinner("generating..."):
+        messages = st.session_state['messages']
+        messages = update_chat(messages, "user", query)
+        response = get_chatgpt_response(messages, model)
+        messages = update_chat(messages, "assistant", response)
+        st.session_state.past.append(query)
+        st.session_state.generated.append(response)
+
+
+if st.session_state['generated']:
+
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+        message(st.session_state["generated"][i], key=str(i))
+
+    with st.expander("Show Messages"):
+        st.write(messages)
---
+++ utils.py
@@ -0,0 +1,78 @@
+import openai
+
+def get_initial_message():
+    messages=[
+            {"role": "system", "content": """You write SQL queries for data analysts at nobroker.
+Given the following table schema for the table "nobroker.base_property" which contains all the properties listed on a website:
+
+    Field name         Data type
+    --------------------------------
+    id                  Text
+    accurate_location    Boolean
+    activate_later       Boolean
+    activation_by        Text
+    activation_date      BigInteger
+    activation_date_partitioned  BigInteger
+    active               Boolean
+    agent                Text
+    available_from       BigInteger
+    base_type            Text
+    bathroom             Integer
+    building_id          Text
+    city                 Text
+    creation_date        BigInteger
+    cup_board            Integer
+    event_time           Text
+    facing               Text
+    floor                Integer
+    furnishing           Text
+    future_activation_date     BigInteger
+    gym                  Boolean
+    inactive_reason      Text
+    last_activation_date BigInteger
+    last_update_date     BigInteger
+    last_updated_by      Text
+    latitude             Float
+    lease_type           Text
+    letout_date          BigInteger
+    lift                 Boolean
+    listing_score        Text
+    locality             Text
+    locality_id          Text
+    longitude            Float
+    managed              Boolean
+    nb_locality          Text
+    negotiable           Boolean
+    owner_id             Text
+    parking              Text
+    pin_code             Integer
+    power_backup         Text
+    property_age         Integer
+    property_code        Text
+    property_size        BigInteger
+    short_url            Text
+    society              Text
+    sponsored            Boolean
+    starship_offset      BigInteger
+    state                Text
+    street               Text
+    swimming_pool        Boolean
+    total_floor          Integer
+    type                 Text
+    water_supply         Text
+
+As an Sql writer you only write the posrgreSQL query and explain it. You don't write anything else."""}
+        ]
+    return messages
+
+def get_chatgpt_response(messages, model="gpt-3.5-turbo"):
+    print("model: ", model)
+    response = openai.ChatCompletion.create(
+    model=model,
+    messages=messages
+    )
+    return  response['choices'][0]['message']['content']
+
+def update_chat(messages, role, content):
+    messages.append({"role": role, "content": content})
+    return messages
